f5e1f2a76ade479445b8b57c7c6a7d813012defc
Bogdan Crisan <bogdan@crbl.studio>
2025-05-19 14:17:18 +0200

    feat(toupper): refactor tool bar style

--- toupper/src/routes/lobby/[lobby]/Buttons.svelte
+++ toupper/src/routes/lobby/[lobby]/Buttons.svelte
@@ -12,34 +12,54 @@
 </script>
 
 <div class="container">
-  <div class="inner-container brush-container">
-    <input id="brush-color" type="color" bind:value={gs.brush.color} />
-    <div
-      class="brush-color"
-      onclick={() => {
-        navigator.clipboard.writeText(gs.brush.color);
-      }}
-    >
-      {gs.brush.color}
+  <div class="inner-group">
+    <div class="inner-container brush-container">
+      <div
+        class="brush-color"
+        onclick={() => {
+          navigator.clipboard.writeText(gs.brush.color);
+        }}
+      >
+        {gs.brush.color}
+      </div>
+      <input id="brush-color" type="color" bind:value={gs.brush.color} />
+    </div>
+    <div class="inner-container">
+      <label for="brush-type">Erase:</label>
+      <input type="checkbox" bind:checked={gs.brush.erase} />
     </div>
   </div>
-  <div class="inner-container">
-    <label for="brush-width">Width:</label>
-    <input id="brush-width" type="range" min="1" max="200" bind:value={gs.brush.width} />
-    <input
-      type="number"
-      oninput={(e) => {
-        if (e.currentTarget.value && !isNaN(Number(e.currentTarget.value))) {
-          gs.brush.width = Number(e.currentTarget.value);
-        }
-      }}
-      value={gs.brush.width}
-    />
-  </div>
-  <div class="inner-container">
-    <label for="brush-diffusion">Diffusion:</label>
-    <input id="brush-diffusion" type="range" min="0" max="100" bind:value={gs.brush.diffusion} />
-    <div>{gs.brush.diffusion}</div>
+  <div class="inner-group">
+    <div class="inner-container">
+      <label for="brush-width">Width:</label>
+      <div class="input-group">
+        <input id="brush-width" type="range" min="1" max="200" bind:value={gs.brush.width} />
+        <input
+          type="number"
+          oninput={(e) => {
+            if (e.currentTarget.value && !isNaN(Number(e.currentTarget.value))) {
+              gs.brush.width = Number(e.currentTarget.value);
+            }
+          }}
+          value={gs.brush.width}
+        />
+      </div>
+    </div>
+    <div class="inner-container">
+      <label for="brush-diffusion">Diffusion:</label>
+      <div class="input-group">
+        <input id="brush-diffusion" type="range" min="0" max="100" bind:value={gs.brush.diffusion} />
+        <input
+          type="number"
+          oninput={(e) => {
+            if (e.currentTarget.value && !isNaN(Number(e.currentTarget.value))) {
+              gs.brush.diffusion = Number(e.currentTarget.value);
+            }
+          }}
+          value={gs.brush.width}
+        />
+      </div>
+    </div>
   </div>
   <div class="inner-container">
     <label for="brush-shape">Shape:</label>
@@ -48,54 +68,55 @@
       <option value={"square"}>Square</option>
     </select>
   </div>
-  <div class="inner-container">
-    <label for="brush-type">Erase:</label>
-    <input type="checkbox" bind:checked={gs.brush.erase} />
-  </div>
-  <div class="inner-container">
-    <label for="zoom">Zoom:</label>
-    <input type="checkbox" bind:checked={gs.zoom} />
-    <input id="zoom" type="range" min="1" max="10" step="0.1" bind:value={gs.zoomRatio} />
-    <input
-      type="number"
-      oninput={(e) => {
-        if (e.currentTarget.value && !isNaN(Number(e.currentTarget.value))) {
-          gs.zoomRatio = Number(e.currentTarget.value);
-        }
-      }}
-      value={gs.zoomRatio}
-    />
-  </div>
-  <div class="inner-container">
-    <label for="background">Background:</label>
-    <input type="checkbox" bind:checked={gs.bg} />
+  <div class="inner-group">
+    <div class="inner-container">
+      <label for="zoom">Zoom:</label>
+      <input type="checkbox" bind:checked={gs.zoom} />
+      <input id="zoom" type="range" min="1" max="10" step="0.1" bind:value={gs.zoomRatio} />
+      <input
+        type="number"
+        oninput={(e) => {
+          if (e.currentTarget.value && !isNaN(Number(e.currentTarget.value))) {
+            gs.zoomRatio = Number(e.currentTarget.value);
+          }
+        }}
+        value={gs.zoomRatio}
+      />
+    </div>
+    <div class="inner-container">
+      <label for="background">Background:</label>
+      <input type="checkbox" bind:checked={gs.bg} />
+    </div>
   </div>
   <div class="inner-container">
     <a class="button" href={saveUrl}>Save DrInFo</a>
   </div>
-  <!-- svelte-ignore a11y_click_events_have_key_events -->
-  <!-- svelte-ignore a11y_no_static_element_interactions -->
   <div
-    class="inner-container button"
-    onclick={() => {
-      const canvas = new OffscreenCanvas(gs.drawing.width, gs.drawing.height);
-      const context = canvas.getContext("2d")!;
-      for (const layerName of gs.drawing.layerOrder) {
-        const layer = gs.drawing.layers.get(layerName)!;
-        for (const key of layer.history.keys().toArray().toSorted()) {
-          const instruction = layer.history.get(key)!;
-          if ("points" in instruction.instruction) {
-            stroke(instruction.instruction, context);
+    class="inner-container">
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div
+      class="button"
+      onclick={() => {
+        const canvas = new OffscreenCanvas(gs.drawing.width, gs.drawing.height);
+        const context = canvas.getContext("2d")!;
+        for (const layerName of gs.drawing.layerOrder) {
+          const layer = gs.drawing.layers.get(layerName)!;
+          for (const key of layer.history.keys().toArray().toSorted()) {
+            const instruction = layer.history.get(key)!;
+            if ("points" in instruction.instruction) {
+              stroke(instruction.instruction, context);
+            }
           }
         }
-      }
-      context.canvas.convertToBlob().then((r) => {
-        const w = window.open("about:blank")!;
-        w.location = URL.createObjectURL(r);
-      });
-    }}
-  >
-    Save PNG
+        context.canvas.convertToBlob().then((r) => {
+          const w = window.open("about:blank")!;
+          w.location = URL.createObjectURL(r);
+        });
+      }}
+    >
+      Save PNG
+    </div>
   </div>
 </div>
 
@@ -103,15 +124,26 @@
   .container {
     display: flex;
     flex-direction: row;
-    gap: 2em;
-    margin: 1em;
+    gap: 1em;
+    padding: .5em;
     flex-wrap: wrap;
+    border-bottom: 1px solid var(--darkGrey);
+  }
+  .inner-group {
+    display: flex;
+    flex-direction: column;
+  }
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    place-items: center;
   }
   .inner-container {
     display: flex;
     flex-direction: row;
     place-items: center;
     gap: 0.4em;
+    justify-content: space-between;
   }
   .inner-container input[type="number"] {
     font-family: monospace;
@@ -119,6 +151,9 @@
     appearance: textfield;
     text-align: right;
     font-size: 14px;
+  }
+  .inner-container input[type="range"] {
+    width: 7.5rem;
   }
   .brush-container {
     gap: 0.5em;
@@ -133,7 +168,8 @@
     color: var(--white);
     text-decoration: none;
     border: 1px solid var(--lightGrey);
-    padding: 0.4em;
+    padding: 0.2em;
+    margin: 0.1em;
     border-radius: 0.2em;
     cursor: pointer;
   }
